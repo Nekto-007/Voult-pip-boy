f84fe9c02a3736df2ced61b852a4f0bfd768a9d8
Nekto-007 <138786107+Nekto-007@users.noreply.github.com>
2023-07-06 17:52:03 +0300

    Add files via upload

--- Game/CreateField.js
+++ Game/CreateField.js
@@ -1,3 +1,33 @@
+import { CreateMenu } from "./CreateMenu.js";
+
 export const createfild = (lvl)=>{
-    console.log(lvl)
+    const main =document.querySelector("main");
+    main.classList.add("null");
+    
+
+    console.log(lvl);
+    let lvltext;
+
+    switch(lvl){
+    case 8:  
+    lvltext="Лёгкий уровень"
+    break;
+    case 12:
+    lvltext="Средний уровень"
+    break;
+    case 16:
+    lvltext="Сложный уровень"
+    break;
+    }
+    const game = document.querySelector(".game");
+    game.style.display = "block";
+
+    const level = game.querySelector(".lvl");
+    level.textContent=lvltext;
+
+    const menu = game.querySelector(".btn");
+    menu.addEventListener('click',()=>{
+        CreateMenu();
+
+    })
 }
--- Game/CreateMenu.js
+++ Game/CreateMenu.js
@@ -1,5 +1,11 @@
 import { createfild } from "./CreateField.js";
 
+const main =document.querySelector("main");
+    main.classList.remove("null");
+
+const game = document.querySelector(".game");
+game.style.display = "none";
+
 export const CreateMenu=()=>{
     const menu = document.querySelector(".menu");
     menu.style.display ="block";
@@ -10,12 +16,15 @@ export const CreateMenu=()=>{
             console.log(el.textContent);
             switch(el.textContent){
                 case "Лёгкий":
+                menu.style.display="none";
                     createfild(8);
                     break;
                 case "Средний":
+                    menu.style.display="none";
                     createfild(12);
                     break;
                 case "Сложный":
+                    menu.style.display="none";
                     createfild(16);
                     break;
             }
